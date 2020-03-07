f93bf79b082344e13517b126115d2230a4d65e02
Dias <Dias64@mail.ru>
2020-03-07 20:38:43 +0300

    homework7 - отображение картинки в списке студентов

--- src/main/resources/db/migration/V3__students_images.sql
+++ src/main/resources/db/migration/V3__students_images.sql
@@ -10,6 +10,12 @@ CREATE TABLE students_images (
 );
 
 INSERT INTO students_images (student_id, path) VALUES
-(1, '1.bmp'),
-(2, ''),
-(3, '');
+(1, 'Igor.png'),
+(1, 'Igor2.png'),
+(2, 'Olga.jpg'),
+(2, 'Olga2.jpg'),
+(3, 'Andrew.jpg'),
+(3, 'Andrew2.jpg'),
+(4, 'Alex.jpg'),
+(4, 'Alex2.jpg'),
+(5, 'Jana.jpg');
